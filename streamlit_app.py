d9142260423ec2de72e3d1edf8274f1a6bdf3f19
Avneet Bajwa <117396436+avneetbajwa14@users.noreply.github.com>
2022-11-03 14:28:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new health diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Yogurt and Cherrios')
+streamlit.text('Spinach Kale Smoothie')
+
+
