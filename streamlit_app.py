b338e7cf2515615e80191f1c93ee039f26ba39df
Avneet Bajwa <117396436+avneetbajwa14@users.noreply.github.com>
2022-11-03 14:42:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ streamlit.title('My Parents new health diner')
 streamlit.header('👌Breakfast Favorites')
 streamlit.text('🍵Yogurt and Cherrios')
 streamlit.text('🥬Spinach Kale Smoothie')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
